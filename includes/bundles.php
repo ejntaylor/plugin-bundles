9ef251b336671b12b4abac16052a91565538cd97
Elliot Taylor <elliot@raison.co>
2014-11-09 20:51:29 +0000

    Integrated bundle install with multiple bundles
    Signed-off-by: Elliot Taylor <elliot@raison.co>

--- includes/bundles.php
+++ includes/bundles.php
@@ -2,37 +2,40 @@
 
 
 	
-function mm_get_plugins($plugins)
-{
+function mm_get_plugins($plugins) {
+	
     $args = array(
             'path' => ABSPATH.'wp-content/plugins/',
             'preserve_zip' => false
     );
 
+echo '<div class="updated">';
+
     foreach($plugins as $plugin)
     {
+	    
 
 echo '<br />';	    
-$pb_plugin_check = ABSPATH.'wp-content/plugins/'. $plugin['install'];
+$pb_plugin_check = ABSPATH.'wp-content/plugins/'. $plugin['plugin_install'];
     if (file_exists($pb_plugin_check))
 {    	
 	
-echo 'plugin already installed';
-echo '<br />';
-echo $pb_plugin_check;
-echo '<br />';
+echo $plugin['plugin_install'] . ' already installed';
 
 
 } else {
+			echo $plugin['plugin_install'] . ' installed';
 
-           mm_plugin_download($plugin['path'], $args['path'].$plugin['name'].'.zip');
-           mm_plugin_unpack($args, $args['path'].$plugin['name'].'.zip');
+           mm_plugin_download($plugin['plugin_path'], $args['plugin_path'].$plugin['plugin_name'].'.zip');
+           mm_plugin_unpack($args, $args['plugin_path'].$plugin['plugin_name'].'.zip');
            	
 }
 
 
-            mm_plugin_activate($plugin['install']);
+            mm_plugin_activate($plugin['plugin_install']);
     }
+    
+    echo '</div>';
 }
 function mm_plugin_download($url, $path) 
 {
@@ -53,7 +56,7 @@ function mm_plugin_unpack($args, $target)
             while($entry = zip_read($zip))
             {
                     $is_file = substr(zip_entry_name($entry), -1) == '/' ? false : true;
-                    $file_path = $args['path'].zip_entry_name($entry);
+                    $file_path = $args['plugin_path'].zip_entry_name($entry);
                     if($is_file)
                     {
                             if(zip_entry_open($zip,$entry,"r")) 
@@ -108,7 +111,7 @@ function mm_plugin_activate($installer)
 
 function pb_plugins_json() {
 	$pb_root = 'http://raison.co/bundles_json/';
-	$pb_file = $_GET['bp_bundle_file'];	
+	$pb_file = isset($_GET['bp_bundle_file']) ? $_GET['bp_bundle_file'] : 'bundles';
 	$pb_path = $pb_root.$pb_file.'.json';
 	//echo $pb_path;	
 
@@ -169,7 +172,35 @@ function bp_activate() {
 
 
 	$pbjson = pb_plugins_json();
-	$pb_plugins = $pbjson[bundle_plugins];
+	$pb_bundle_number = isset($_GET['bundle_number']) ? $_GET['bundle_number'] : '';
+	if ($pb_bundle_number == '') {
+		echo 'no bundle found';
+		return;
+		}
+	echo 'Activating and Installing' . $pb_bundle_number;
+	$pb_plugins = $pbjson[$pb_bundle_number][bundle_plugins];
+
+	var_dump($pb_plugins);
+
+						
+			mm_get_plugins($pb_plugins);
+		
+	
+}
+
+
+function bp_deactivate() {
+	echo '<h2>Deactivate</h2>';
+}		
+		
+		
+		
+// Loop Logic
+
+function bp_loop_logic() {
+	
+	$pbjson = pb_plugins_json();
+	$pb_plugins = $pbjson;
 	//var_dump($pb_plugins);
 
 	if ($pb_plugins == null) {
@@ -179,31 +210,78 @@ function bp_activate() {
 	
 	else {
 		
-	//	var_dump($pb_plugins);
 		
 		foreach ($pb_plugins as $pb_plugin_item_number => $pb_plugin_item_details ) {
-		
-		echo $pb_plugin_item_number;	
-		echo '<br>'; 	
-		var_dump($pb_plugin_item_details);
-		echo '<br>'; 	
-			
-		//$pb_bundle_go = $pbjson[bundle_plugins]
-		//mm_get_plugins($pb_bundle_go);
+
+			bp_loop_item($pb_plugin_item_details, $pb_plugin_item_number);
+						
+			//$pb_bundle_go = $pbjson[bundle_plugins]
+			//mm_get_plugins($pb_bundle_go);
 		}
-	}
+		
 }
 
-
-function bp_deactivate() {
-	echo '<h2>Deactivate</h2>';
 }		
+		
+		
+		
+// Loop Output
+
+function bp_loop_item($pb_data, $pb_number) { 
+	
+//var_dump($pb_data); 
+
+	
+?>
+
+
+<div id="bundle-section">
+	<ul id="bundle-list">
+		<li class="bundle-item">
+			<div class="bundle-item-l">
+				<div class="bundle-title-top">
+					<h3><?php echo $pb_data[bundle_info][bundle_name]; ?></h3>
+					<div class="plugin-info"><?php echo count($pb_data[bundle_plugins]); ?> Plugins by <span><a href="<?php echo $pb_data[bundle_info][bundle_author_link]; ?>"><?php echo $pb_data[bundle_info][bundle_author]; ?></a></span></div>
+				</div>
+				<div class="bundle-description"><?php echo $pb_data[bundle_info][bundle_description]; ?></div>
+								
+				<div class="activate">
+					<form>
+						<input type="hidden" name="bundle_number" value="<?php echo $pb_number; ?>">
+						<input type="hidden" name="action" value="activate">
+						<input type="hidden" name="page" value="bundles">
+						<input type="submit" value="Download and install" />
+					
+					</form>
+				</div>
+				
+			</div>
+			<div class="bundle-item-r">
+				ICONS
+			</div>
+		</li>
+	</ul>
+</div>
+
+
+<?php }	
+		
+		
+		
+// page loaded
+
+bp_loop_logic();
+
 		
 ?>
 
 
 
 
+<!--
+
+
+// Use for Custom Fetch
 
 <form>
 	<input class="" type="text" name="bp_bundle_file" value="bundles" checked="checked">Text
@@ -212,9 +290,5 @@ function bp_deactivate() {
 	<input type="submit" value="Install" />
 
 </form>
+-->
 
-
-
-
-
-<?php include_once('bundles_page.php'); ?>
