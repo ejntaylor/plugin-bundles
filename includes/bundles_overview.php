599dcb4dcd16844ed017d1d9028e40e0123a6534
Elliot Taylor <elliot@raison.co>
2014-11-20 13:13:43 +0000

    Tidy
    Signed-off-by: Elliot Taylor <elliot@raison.co>

--- includes/bundles_overview.php
+++ includes/bundles_overview.php
@@ -3,7 +3,7 @@
 
 
 /**
- * bp_api_query function.
+ * Query the WP API for Plugin Information
  *
  * Returns the API object from the plugin slug
  * ref: http://stackoverflow.com/questions/10353859/is-it-possible-to-programmatically-install-plugins-from-wordpress-theme
@@ -40,6 +40,61 @@ function bp_api_query($slug) {
 	return $call_api;
 
 
+}
+
+
+
+/**
+ * WP API Function
+ *
+ * Access information about plugin from the API
+ *
+ * @access public
+ * @param mixed $action
+ * @param mixed $args (default: null)
+ * @return string
+ */
+
+
+function bp_wpapi($action, $args = null) {
+
+	if ( is_array($args) )
+		$args = (object)$args;
+
+	if ( !isset($args->per_page) )
+		$args->per_page = 24;
+
+	// Allows a plugin to override the WordPress.org API entirely.
+	// Use the filter 'plugins_api_result' to merely add results.
+	// Please ensure that a object is returned from the following filters.
+	$args = apply_filters('plugins_api_args', $args, $action);
+	$res = apply_filters('plugins_api', false, $action, $args);
+
+	if ( false === $res ) {
+		$url = 'http://api.wordpress.org/plugins/info/1.0/';
+		if ( wp_http_supports( array( 'ssl' ) ) )
+			$url = set_url_scheme( $url, 'https' );
+
+		$request = wp_remote_post( $url, array(
+				'timeout' => 15,
+				'body' => array(
+					'action' => $action,
+					'request' => serialize( $args )
+				)
+			) );
+
+		if ( is_wp_error($request) ) {
+			$res = new WP_Error('plugins_api_failed', __( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="http://wordpress.org/support/">support forums</a>.' ), $request->get_error_message() );
+		} else {
+			$res = maybe_unserialize( wp_remote_retrieve_body( $request ) );
+			if ( ! is_object( $res ) && ! is_array( $res ) )
+				$res = new WP_Error('plugins_api_failed', __( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="http://wordpress.org/support/">support forums</a>.' ), wp_remote_retrieve_body( $request ) );
+		}
+	} elseif ( !is_wp_error($res) ) {
+		$res->external = true;
+	}
+
+	return apply_filters('plugins_api_result', $res, $action, $args);
 }
 
 
@@ -311,60 +366,6 @@ function bp_loop_logic() {
 
 
 
-/**
- * bp_wpapi function.
- *
- * Access information about plugin from the API
- *
- * @access public
- * @param mixed $action
- * @param mixed $args (default: null)
- * @return string
- */
-
-
-function bp_wpapi($action, $args = null) {
-
-	if ( is_array($args) )
-		$args = (object)$args;
-
-	if ( !isset($args->per_page) )
-		$args->per_page = 24;
-
-	// Allows a plugin to override the WordPress.org API entirely.
-	// Use the filter 'plugins_api_result' to merely add results.
-	// Please ensure that a object is returned from the following filters.
-	$args = apply_filters('plugins_api_args', $args, $action);
-	$res = apply_filters('plugins_api', false, $action, $args);
-
-	if ( false === $res ) {
-		$url = 'http://api.wordpress.org/plugins/info/1.0/';
-		if ( wp_http_supports( array( 'ssl' ) ) )
-			$url = set_url_scheme( $url, 'https' );
-
-		$request = wp_remote_post( $url, array(
-				'timeout' => 15,
-				'body' => array(
-					'action' => $action,
-					'request' => serialize( $args )
-				)
-			) );
-
-		if ( is_wp_error($request) ) {
-			$res = new WP_Error('plugins_api_failed', __( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="http://wordpress.org/support/">support forums</a>.' ), $request->get_error_message() );
-		} else {
-			$res = maybe_unserialize( wp_remote_retrieve_body( $request ) );
-			if ( ! is_object( $res ) && ! is_array( $res ) )
-				$res = new WP_Error('plugins_api_failed', __( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="http://wordpress.org/support/">support forums</a>.' ), wp_remote_retrieve_body( $request ) );
-		}
-	} elseif ( !is_wp_error($res) ) {
-		$res->external = true;
-	}
-
-	return apply_filters('plugins_api_result', $res, $action, $args);
-}
-
-
 
 
 /**
@@ -514,7 +515,7 @@ function bp_loop_item($pb_data, $pb_number) {
 		// output the plugin list item
 
 		//var_dump($pb_plugin_item );
-		//$bp_api_name = bp_api_query($pb_plugin_item[plugin_slug]);
+		$bp_api_name = bp_api_query($pb_plugin_item[plugin_slug]);
 
 		echo '<div class="bundle-plugin-item"><a href="'.$bp_api_name->homepage .'" target="blank"><div '. $bgImage . ' class="bundle-icon"></div><div class="bundle-item-right"><div class="bundle-name">' . $bp_api_name->name .'</div><div class="bundle-rating">Rating: ' . $bp_api_name->rating  . '</div></div></a></div>';
 
